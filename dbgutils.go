ad9b46b41ee583933ac619756b2df51a12d1160f
chuacw <40189902+EximChua@users.noreply.github.com>
2018-12-05 14:24:23 +0800

    Updated how backup nodes are selected
    Updated how backup nodes are selected.
    Validator, Maker, then Observer.

--- dbgutils.go
+++ dbgutils.go
@@ -6,8 +6,10 @@ import (
 	"strings"
 )
 
+// UNUSED marks anything as unused
 func UNUSED(intf interface{}) {}
 
+// DumpEnvironment dumps the environment
 func DumpEnvironment() {
 	for _, e := range os.Environ() {
 		pair := strings.Split(e, "=")
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math/rand"
 	"net"
@@ -18,15 +19,15 @@ import (
 	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
 var (
+	// BuildDate allows the external linker to set the build date when building
 	BuildDate string
 )
 
 type (
-	// Message from SNS topic
+	// AWSBackupMessage is the message from SNS topic
 	AWSBackupMessage struct {
 		Version    string    `json:"version"`
 		ID         string    `json:"id"`
@@ -41,7 +42,7 @@ type (
 	}
 )
 
-func getNetworkId() (result string) {
+func getNetworkID() (result string) {
 	result = os.Getenv("NetworkId")
 	if result == "" {
 		for _, e := range os.Environ() {
@@ -67,29 +68,14 @@ func getRegion() (result string) {
 	return
 }
 
+// NewConfig creates an empty aws.Config
 func NewConfig() (result *aws.Config) {
 	return &aws.Config{}
 }
 
-func Run(svcSsm *ssm.SSM, instanceId, DNSName, command string) {
-	param := make(map[string][]*string)
-	param["commands"] = aws.StringSlice([]string{command})
-	sendCommandInput := &ssm.SendCommandInput{
-		Comment:      aws.String(command),
-		DocumentName: aws.String("AWS-RunShellScript"),
-		Parameters:   param,
-		InstanceIds:  aws.StringSlice([]string{instanceId}),
-	}
-
-	if sendCommandOutput, err := svcSsm.SendCommand(sendCommandInput); err == nil {
-		fmt.Printf("Command executed successfully on instance: %v, DNS: %s\n", instanceId, DNSName)
-		fmt.Printf("Response: %v\n", sendCommandOutput)
-	} else {
-		fmt.Printf("Failed to execute command due to %v\n", err)
-	}
-}
-
+// ShowIPAddress shows the IP address
 func ShowIPAddress() {
+	var sb strings.Builder
 	if ifaces, err := net.Interfaces(); err == nil {
 		// handle err
 		for _, i := range ifaces {
@@ -103,20 +89,63 @@ func ShowIPAddress() {
 					case *net.IPAddr:
 						ip = v.IP
 					}
-					// process IP address
-					fmt.Println("IP: ", ip)
+					if sb.Len() > 0 {
+						sb.WriteString(", ")
+					}
+					sb.WriteString(ip.String())
 				}
 			}
 		}
 	}
+	// process IP address
+	if sb.Len() > 0 {
+		fmt.Printf("IP: %s\n", sb.String())
+	}
+
 }
 
+func makeFilter(NetworkID, role string) (result []*ec2.Filter) {
+	result = []*ec2.Filter{
+		// This is the NetworkId that I'm interested in
+		&ec2.Filter{
+			Name:   aws.String("tag:NetworkId"),
+			Values: aws.StringSlice([]string{NetworkID}),
+		},
+		// These are the roles that are running
+		&ec2.Filter{
+			Name: aws.String("tag:Role"),
+			Values: aws.StringSlice([]string{
+				role,
+			}),
+		},
+		// Look at only running instances
+		&ec2.Filter{
+			Name:   aws.String("instance-state-name"),
+			Values: aws.StringSlice([]string{"running"}),
+		},
+	}
+	return
+}
+
+// Returns the nodes by order of definition
+func getFilteredResponse(responses [3]*ec2.DescribeInstancesOutput, nodetypes []string, errs [3]error) (response *ec2.DescribeInstancesOutput, nodetype string, err error) {
+	for i := 0; i < 3; i++ {
+		if errs[i] == nil && responses[i] != nil && len(responses[i].Reservations) > 0 {
+			return responses[i], nodetypes[i], errs[i]
+		}
+	}
+	return nil, "", errors.New("No instances")
+}
+
+// BackupHandler is the handler for AWS Lambda
 func BackupHandler(ctx context.Context, snsEvent events.SNSEvent) {
 	defer func() { fmt.Println("Terminated.") }()
-	NetworkId := getNetworkId()
+	NetworkID := getNetworkID()
 	t := time.Now()
-	fmt.Println("Compiled on ", BuildDate)
-	fmt.Printf("Scheduled backup event for %s at %s\n", NetworkId, t.String())
+	if BuildDate != "" {
+		fmt.Println("Compiled on ", BuildDate)
+	}
+	fmt.Printf("Scheduled backup event for %s at %s\n", NetworkID, t.String())
 	ShowIPAddress()
 
 	var (
@@ -133,85 +162,83 @@ func BackupHandler(ctx context.Context, snsEvent events.SNSEvent) {
 		}
 		sess = session.Must(session.NewSessionWithOptions(opts))
 	}
-	downloader := s3manager.NewDownloader(sess)
-
-	getS3BucketObj := func(downloader *s3manager.Downloader, bucketName, bucketKey string) (result []byte, err error) {
-		var n int64
-		buffer := []byte{}
-		buf := aws.NewWriteAtBuffer(buffer)
-		fmt.Printf("Downloading %s %s\n", bucketName, bucketKey)
-		if n, err = downloader.Download(buf, &s3.GetObjectInput{
-			Bucket: aws.String(bucketName),
-			Key:    aws.String(bucketKey),
-		}); err == nil {
-			result = buf.Bytes()
-			fmt.Printf("Bytes read: %v for bucket: %s, key: %s\n", n, bucketName, bucketKey)
-		} else {
-			fmt.Printf("Bucket: %s, Item: %s, err: %v\n", bucketName, bucketKey, err)
-		}
-		return
-	}
 
 	myBucket := getPrintEnv("Bucket")
 	myKey := getPrintEnv("Key")
 	sshUser := getPrintEnv("SSHUser")
+	sshPass := getPrintEnv("SSHPass")
 
-	if encryptedData, err := getS3BucketObj(downloader, myBucket, myKey); err == nil {
-		input := &kms.DecryptInput{
-			CiphertextBlob: encryptedData,
-		}
-		kmssvc := kms.New(sess)
-		result, err := kmssvc.Decrypt(input)
-		if err != nil {
-			fmt.Println("Decryption failed.")
-			handleErr(err)
+	if sshPass == "" {
+		downloader := s3manager.NewDownloader(sess)
+		getS3BucketObj := func(downloader *s3manager.Downloader, bucketName, bucketKey string) (result []byte, err error) {
+			var n int64
+			buffer := []byte{}
+			buf := aws.NewWriteAtBuffer(buffer)
+			fmt.Printf("Downloading %s %s\n", bucketName, bucketKey)
+			if n, err = downloader.Download(buf, &s3.GetObjectInput{
+				Bucket: aws.String(bucketName),
+				Key:    aws.String(bucketKey),
+			}); err == nil {
+				result = buf.Bytes()
+				fmt.Printf("Bytes read: %v for bucket: %s, key: %s\n", n, bucketName, bucketKey)
+			} else {
+				fmt.Printf("Bucket: %s, Item: %s, err: %v\n", bucketName, bucketKey, err)
+			}
 			return
-		} else {
+		}
+
+		if encryptedData, err := getS3BucketObj(downloader, myBucket, myKey); err == nil {
+			input := &kms.DecryptInput{
+				CiphertextBlob: encryptedData,
+			}
+			kmssvc := kms.New(sess)
+			result, err := kmssvc.Decrypt(input)
+			if err != nil {
+				fmt.Println("Decryption failed.")
+				handleErr(err)
+				return
+			}
 			sshKey = result.Plaintext
 			fmt.Printf("Decryption succeeded, length: %d\n", len(sshKey))
+		} else {
+			fmt.Printf("Unable to retrieve S3 bucket: %s key: %s due to error: %v\n", myBucket, myKey, err)
+			return
 		}
-	} else {
-		fmt.Printf("Unable to retrieve S3 bucket: %s key: %s due to error: %v\n", myBucket, myKey, err)
-		return
 	}
 
 	svc := ec2.New(sess)
 
-	filters := []*ec2.Filter{
-		// This is the NetworkId that I'm interested in
-		&ec2.Filter{
-			Name:   aws.String("tag:NetworkId"),
-			Values: aws.StringSlice([]string{NetworkId}),
-		},
-		// These are the roles that are running
-		&ec2.Filter{
-			Name: aws.String("tag:Role"),
-			Values: aws.StringSlice([]string{
-				"Maker",
-			}),
-		},
-		// Look at only running instances
-		&ec2.Filter{
-			Name:   aws.String("instance-state-name"),
-			Values: aws.StringSlice([]string{"running"}),
-		},
+	var (
+		filters   [3][]*ec2.Filter
+		responses [3]*ec2.DescribeInstancesOutput
+		errs      [3]error
+	)
+	filterNames := []string{"Validator", "Maker", "Observer"}
+	for i, filterName := range filterNames {
+		filters[i] = makeFilter(NetworkID, filterName)
+		filtersInput := &ec2.DescribeInstancesInput{Filters: filters[i]}
+		responses[i], errs[i] = svc.DescribeInstances(filtersInput)
 	}
-	input := &ec2.DescribeInstancesInput{Filters: filters}
 
 	fmt.Println("Retrieving instances...")
-	if resp, err := svc.DescribeInstances(input); err == nil {
+	if resp, nodetype, err := getFilteredResponse(responses, filterNames, errs); err == nil {
 		numInst := len(resp.Reservations)
 		lucky := rand.Intn(numInst)
 		idx := lucky
 
-		fmt.Printf("Number of instances: %d, chose %d\n", numInst, idx+1)
+		fmt.Printf("Number of instances: %d, chose %d, node type: %s\n", numInst, idx+1, nodetype)
 
 		for _, inst := range resp.Reservations[idx].Instances {
 			DNSName := aws.StringValue(inst.PublicDnsName)
-			InstanceId := aws.StringValue(inst.InstanceId)
-			fmt.Printf("Instance: %s, DNS: %s\n", InstanceId, DNSName)
+			InstanceID := aws.StringValue(inst.InstanceId)
+			fmt.Printf("Instance: %s, DNS: %s\n", InstanceID, DNSName)
 
-			sshconfig := softwareupgrade.NewSSHConfigKeyBytes(sshUser, sshKey, DNSName)
+			var sshconfig *softwareupgrade.SSHConfig
+			if sshPass != "" {
+				sshconfig = softwareupgrade.NewSSHConfigPassword(sshUser, sshPass, DNSName)
+			} else {
+				sshconfig = softwareupgrade.NewSSHConfigKeyBytes(sshUser, sshKey, DNSName)
+			}
 			if err := sshconfig.Connect(); err != nil {
 				fmt.Printf("Unable to connect to %s due to error: %v\n", DNSName, err)
 				continue
@@ -234,6 +261,7 @@ func BackupHandler(ctx context.Context, snsEvent events.SNSEvent) {
 
 }
 
+// DebugEnvironment returns true if the environment indicates it is run in debug mode
 func DebugEnvironment() (result bool) {
 	funcName := os.Getenv("AWS_LAMBDA_FUNCTION_NAME")
 	vsCodeLogs := os.Getenv("VSCODE_LOGS")
